fc26e09e129e77aad2b376073b4e94b36f8cb804
Junliang HU <jlhu@cse.cuhk.edu.hk>
2022-12-18 09:06:12 +0800

    add maven.sh

---
+++ maven.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ex
+
+DIR="$(dirname "$(readlink -f "$0")")")
+MVN="$DIR/apache-maven-3.8.6/bin/mvn"
+MVN_RELEASE="https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz"
+export MAVEN_OPTS="-Xmx$((2*$(nproc)))g"
+
+cd "$DIR"
+[ -f "$MVN" ] || ( wget "$MVN_RELEASE" && tar axvf apache-maven-3.8.6-bin.tar.gz )
+
+
+"$MVN" compile exec:java \
+  -Dexec.cleanupDaemonThreads=false \
+  -Dexec.topInfoboxPropertiesPerLang=None \
+  -Dexec.mainClass="uk.co.gresearch.dgraph.dbpedia.DbpediaToParquetSparkApp" \
+  -Dexec.args="dbpedia 2016-10"
+
+"$MVN" compile exec:java \
+  -Dexec.cleanupDaemonThreads=false \
+  -Dexec.topInfoboxPropertiesPerLang=None \
+  -Dexec.mainClass="uk.co.gresearch.dgraph.dbpedia.DbpediaDgraphSparkApp" \
+  -Dexec.args="dbpedia 2016-10"
+
