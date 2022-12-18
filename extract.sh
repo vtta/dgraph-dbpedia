f9987a858ed85462adf2531401a16b8cf3d52ceb
Junliang HU <jlhu@cse.cuhk.edu.hk>
2022-12-18 09:05:52 +0800

    use fd instead of find

--- extract.sh
+++ extract.sh
@@ -15,8 +15,10 @@ extract=$(dirname "$0")
 
 # extract files in parallel and in reverse file size order
 #find -L "$base" -name "*.$EXT" -exec ls -s "{}" ";" | sort -nr | cut -d " " -f 2- | while read file; do if [ ! -e "${file/%.$EXT/}" ]; then echo $file; fi; done | sort | parallel --will-cite --progress --eta $extract/extract_file.sh
+# find -L "$base" -name "*.$EXT" | while read file; do if [ ! -e "${file/%.$EXT/}" ]; then echo $file; fi; done | sort | while read file
+# do
+# 	$extract/extract_file.sh "$file"
+# done
 
-find -L "$base" -name "*.$EXT" | while read file; do if [ ! -e "${file/%.$EXT/}" ]; then echo $file; fi; done | sort | while read file
-do
-	$extract/extract_file.sh "$file"
-done
+fd -e bz2 . "$base" | parallel --will-cite --progress --eta bunzip2 -dk {}
+
