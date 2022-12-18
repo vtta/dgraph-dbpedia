ebec23480f545a99dd5484ffe1ff7532772e46f3
Junliang HU <jlhu@cse.cuhk.edu.hk>
2022-12-18 09:29:51 +0800

    update java

--- maven.sh
+++ maven.sh
@@ -5,9 +5,13 @@ DIR="$(dirname "$(readlink -f "$0")")"
 MVN="$DIR/apache-maven-3.8.6/bin/mvn"
 MVN_RELEASE="https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz"
 export MAVEN_OPTS="-Xmx$((2*$(nproc)))g"
+JAVA_RELEASE="https://cfdownload.adobe.com/pub/adobe/coldfusion/java/java8/java8u351/jdk/jdk-8u351-linux-x64.tar.gz"
+JAVA="$DIR/jdk1.8.0_351/bin/java"
+export JAVA_HOME="$DIR/jdk1.8.0_351"
 
 cd "$DIR"
 [ -f "$MVN" ] || ( wget "$MVN_RELEASE" && tar axvf apache-maven-3.8.6-bin.tar.gz )
+[ -f "$JAVA" ] || ( wget "$JAVA_RELEASE" && tar axvf jdk-8u351-linux-x64.tar.gz )
 
 
 "$MVN" compile exec:java \
